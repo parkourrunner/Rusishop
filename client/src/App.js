338f153f72b588ef76f3b43ea50933c36ff3efb3
Ali Salehi <parkour.runner@gmail.com>
2022-11-16 18:05:06 +0300

    donts added working on header

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import Shop from "./Pages/Shop";
-
 export default function App() {
   return (
     <>
-      <Shop />
+      <Routes>
+        <Route path="/" element={<Shop />} />
+      </Routes>
+
       <h1>روسی شاپ</h1>
     </>
   );
--- client/src/Layouts/Header/index.js
+++ client/src/Layouts/Header/index.js
@@ -1,7 +1,64 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faEnvelope,
+  faSearch,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
 
+import "./index.scss";
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header className="top-header-bar-container w-100">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <ul className="top-header-bar">
+              <li className="top-email">
+                <FontAwesomeIcon icon={faEnvelope} />
+                admin@rusishop.com
+              </li>
+              <li className="top-account">
+                <Link to="/">
+                  <FontAwesomeIcon icon={faUser} />
+                  حساب کاربری
+                </Link>
+              </li>
+              <li className="top-search">
+                <form
+                  role="search"
+                  method="get"
+                  class="pull-right"
+                  id="searchform_topbar"
+                  action="https://www.rusishop.com/"
+                >
+                  <label>
+                    <span class="screen-reader-text"></span>
+                    <input
+                      class="search-field-top-bar"
+                      id="search-field-top-bar"
+                      placeholder="جستجو کنید..."
+                      value=""
+                      name="s"
+                      type="search"
+                    />
+                  </label>
+                  <button
+                    id="search-top-bar-submit"
+                    type="submit"
+                    class="search-top-bar-submit"
+                  >
+                    <FontAwesomeIcon icon={faSearch} />
+                  </button>
+                </form>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
